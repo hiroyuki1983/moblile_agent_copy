122d534d8e68581bc824e2bb4c779b78a14149ec
hiroyuki1983 <hiroyuki1983@gmail.com>
2017-04-24 02:06:28 +0900

    連絡先一覧用のJSファイルをとりあえず作った。

---
+++ www/script/renrakusaki.js
@@ -0,0 +1,10 @@
+// This is a JavaScript file
+
+// http://qiita.com/miiitaka/items/f77cb83b9dfcd05aea82
+
+// 想定するAPIペイロード
+// [
+//     {"id":1, "picture":"kao1.png", name":"名前", "telNum1":"11111", "telNum2":22222, "mail":"hoge@piyo.com", "url":"http://hoge.com", "comment":"コメントコメント"},
+//     {"id":2, "picture":"kao1.png", name":"名前", "telNum1":"11111", "telNum2":22222, "mail":"hoge@piyo.com", "url":"http://hoge.com", "comment":"コメントコメント"},
+//     {"id":3, "picture":"kao1.png", name":"名前", "telNum1":"11111", "telNum2":22222, "mail":"hoge@piyo.com", "url":"http://hoge.com", "comment":"コメントコメント"}
+// ]
